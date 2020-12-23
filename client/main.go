c10f22d3b054010b3fe7b5bfc0d2ff580157d38d
col <>
2020-12-23 13:59:02 +0900

    start

--- client/main.go
+++ client/main.go
@@ -19,35 +19,6 @@ func main() {
 
 	receiveHandler(client)
 	sendHandler(client)
-	//go func(c net.Conn) {
-	//	data := make([]byte, 4096)
-	//
-	//	for {
-	//		n, err := c.Read(data)
-	//		if err != nil {
-	//			fmt.Println(err)
-	//			return
-	//		}
-	//
-	//		fmt.Println(string(data[:n]))
-	//		time.Sleep(1 * time.Second)
-	//	}
-	//}(client)
-
-	//go func(c net.Conn) {
-	//	i := 0
-	//	for {
-	//		s := "Hello " + strconv.Itoa(i)
-	//
-	//		_, err := c.Write([]byte(s)) // 서버로 부터 데이터 전송
-	//		if err != nil {
-	//			fmt.Println(err)
-	//			return
-	//		}
-	//		i++
-	//		time.Sleep(1 * time.Second)
-	//	}
-	//}(client)
 	//var input string
 	//fmt.Scanln(&input)
 }
